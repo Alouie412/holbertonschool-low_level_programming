ef7c9e7ca9a0f76087809ae0f384501cbcf89cfe
Fazel412 <alouie412@gmail.com>
2019-09-24 16:12:18 +0000

    Removed unnecessary code

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-  char letter[] = "Holberton";
-  int i;
+	char letter[] = "Holberton";
+	int i;
 
-  for (i = 0; i < 10; i++)
-    {
-      _putchar(letter[i]);
-    }
+	for (i = 0; i < 10; i++)
+	{
+	 _putchar(letter[i]);
+	}
 
-  _putchar('\n');
+	_putchar('\n');
 
-  return (0);
+	return (0);
 }
