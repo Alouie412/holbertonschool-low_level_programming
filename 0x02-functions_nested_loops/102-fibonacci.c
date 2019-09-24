fa90914646e9d96a29eaf51476f2f18a47396d92
Fazel412 <alouie412@gmail.com>
2019-09-24 18:52:13 +0000

    Fixing code

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,22 +7,24 @@
  */
 int main(void)
 {
-  long a = 1, b = 2, f = 0, i = 0;
+	long a = 1, b = 2, f = 0, i = 0;
 
-  printf("%li, %li, ", a, b);
+	printf("%li, %li, ", a, b);
 
-  for (i; i < 48; i++)
-    {
-      f = a + b;
-      printf("%li", f);
-      if (i % 2 == 0)
-	a = f;
-      else
-	b = f;
+	for (i; i < 47; i++)
+	{
+		f = a + b;
+		printf("%li", f);
 
-      if (i != 47)
-	printf(", ");
-    }
-  printf("\n");
-  return (0);
+		if (i % 2 == 0)
+		a = f;
+		else
+		b = f;
+
+		if (i != 46)
+		printf(", ");
+	}
+
+	printf("\n");
+	return (0);
 }
