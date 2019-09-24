163ebaef621598ab378039619c1d71b2fa66d217
Fazel412 <alouie412@gmail.com>
2019-09-24 19:14:14 +0000

    Fixed typo

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,30 +8,30 @@
  */
 void print_to_98(int n)
 {
-  if (n == 98)
-    {
-      printf("%d\n", n);
-    }
-  else if(n < 98)
-    {
-      for(; n <= 98; n++)
+	if (n == 98)
 	{
-	  printf("%d", n);
-	  if (n != 98)
-	    printf(", ");
-	  else
-	    printf("\n");
+		printf("%d\n", n);
 	}
-    }
-  else if (n > 98)
-    {
-      for(; n >= 98; n--)
+	else if (n < 98)
 	{
-	  printf("%d", n);
-	  if (n != 98)
-	    printf(", ");
-	  else
-	    printf("\n");
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			else
+				printf("\n");
+		}
 	}
-    }
+	else if (n > 98)
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			else
+				printf("\n");
+		}
+	}
 }
