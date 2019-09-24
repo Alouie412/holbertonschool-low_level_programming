85953c089f027b26f1ecf00ee822b80cd8ac9678
Fazel412 <alouie412@gmail.com>
2019-09-24 16:22:13 +0000

    Editing format

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
