83790e1d1ffc6a61772d9907abfb41db55e081d6
Fazel412 <alouie412@gmail.com>
2019-09-25 06:11:43 +0000

    Data type change

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,21 +6,24 @@
  */
 int main(void)
 {
-  unsigned long a = 1, b = 2, f = 0;
-  int i;
+	double a = 1, b = 2;
+	int i;
 
-  printf("%lu, %lu, ", a, b);
-  for (i = 0; i < 95; i++)
-    {
-      f = a + b;
-      if (i % 2 == 0)
-	a = f;
-      else
-	b = f;
-      printf("%lu", f);
-      if (i != 94)
-	printf(", ");
-    }
-  printf("\n");
-  return (0);
+	printf("%.0lf, %.0lf, ", a, b);
+
+	for (i = 0; i < 96; i++)
+	{
+		printf("%.0lf", a + b);
+
+		if (i % 2 == 0)
+			a = a + b;
+		else
+			b = a + b;
+
+		if (i != 95)
+		printf(", ");
+	}
+
+	printf("\n");
+	return (0);
 }
