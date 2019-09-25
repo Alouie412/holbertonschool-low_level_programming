23889536ed77e2a229c3fc101caed100d04fa20f
Fazel412 <alouie412@gmail.com>
2019-09-25 01:21:18 +0000

    Logic editing

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -26,18 +26,18 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-		  if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+			if (year % 400 != 0 || year % 4 != 0)
 				printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 			else
 			{
 				printf("Day of the year: %d\n", day);
 				printf("Remaining days: %d\n", 366 - day);
 			}
-        	}
+		}
 		else
-        	{
+		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
-        	}
+		}
 	}
 }
