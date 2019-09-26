00275851be5ae45db80f0e1538ad90809fc9d2ac
Fazel412 <alouie412@gmail.com>
2019-09-26 19:30:59 +0000

    Changed logic

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,9 +14,9 @@ int main(void)
 	{
 		if (num % 15 == 0)
 			printf("FizzBuzz ");
-		else if (num % 3 == 0)
+		else if ((num % 3) == 0)
 			printf("Fizz ");
-		else if (num % 5 == 0)
+		else if ((num % 5) == 0)
 			printf("Buzz ");
 		else
 			printf("%d ", num);
