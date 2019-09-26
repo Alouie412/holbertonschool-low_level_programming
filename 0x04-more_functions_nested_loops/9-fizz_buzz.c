01060f94bb4b0de07a28de15ffd695bc463995e2
Fazel412 <alouie412@gmail.com>
2019-09-26 19:02:51 +0000

    Fixed typo

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,7 @@ int main(void)
 	for (num = 1; num <= 100; num++)
 	{
 		if (num % 3 == 0 && num % 5 == 0)
-			printf("Fizzbuzz");
+			printf("FizzBuzz");
 		else if (num % 3 == 0)
 			printf("Fizz");
 		else if (num % 5 == 0)
