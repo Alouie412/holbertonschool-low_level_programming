ba0680a3782935b2583d66264f03a8dd72829374
Fazel412 <alouie412@gmail.com>
2019-10-01 17:43:45 +0000

    Betty style fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * reset_to_98 - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 void reset_to_98(int *n)
 {
-  *n = 98;
+	*n = 98;
 }
