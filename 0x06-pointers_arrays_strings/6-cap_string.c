98b51db9131459c06cd18a61d7081ad7949b28d0
Fazel412 <alouie412@gmail.com>
2019-10-03 23:54:08 +0000

    Logic fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,34 +19,24 @@ char *cap_string(char *str)
 		if (counter == 0)
 		{
 			if (str[j] >= 'a' && str[j] <= 'z')
-			{
 				str[j] = str[j] - 32;
-			}
 			else
 				counter++;
 		}
 
 		if (str[j] == '!' || str[j] == '"' || str[j] == '(' || str[j] == ')')
-		{
 			counter = 0;
-		}
 		else if (str[j] == ',' || str[j] == '.' || str[j] == '{' || str[j] == '}')
-		{
 			counter = 0;
-		}
 		else if (str[j] == ';' || str[j] == '?' || str[j] == '\n')
-		{
 			counter = 0;
-		}
 		else if (str[j] == '\t')
 		{
 			counter = 0;
 			str[j] = ' ';
 		}
 		else if (str[j] == ' ')
-		{
 			counter = 0;
-		}
 		else
 			counter++;
 	}
