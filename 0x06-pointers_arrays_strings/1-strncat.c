bd09380bb9830c17dafeba3ff2f713a98cb1430c
Fazel412 <alouie412@gmail.com>
2019-10-03 21:54:44 +0000

    Total rewrite

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * _strncat - check the code for Holberton School students.
@@ -11,16 +12,11 @@ char *_strncat(char *dest, char *src, int n)
 {
   int i, j;
 
-  i = 0;
-  while (src[i] != '\0')
-    i++;
+  for (i = 0; dest[i] != '\0'; i++);
 
-  for (j = 0; j < n && dest[j] != '\0'; j++)
+  for (j = 0; j < n; j++, i++)
     {
-      if (n == 1)
-	dest[j + i - 1] = src[j];
-      else
-	dest[j + i] = src[j];
+      dest[i] = src[j];
     }
 
   return (dest);
