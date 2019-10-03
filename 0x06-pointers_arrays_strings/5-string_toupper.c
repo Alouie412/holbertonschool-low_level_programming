e407b8f5bd2191962b546f89c879cd597c387e70
Fazel412 <alouie412@gmail.com>
2019-10-03 21:44:27 +0000

    Betty style fix

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,28 +1,28 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * string_toupper - check the code for Holberton School students.
+ * @str: String to be reversed
  * Return: Always 0.
  */
 char *string_toupper(char *str)
 {
-  int i, j;
-  char letter;
+	int i, j;
+	char letter;
 
-  i = 0;
-  while (str[i] != '\0')
-    i++;
+	i = 0;
+	while (str[i] != '\0')
+		i++;
 
-  for (j = 0; j < i; j++)
-    {
-      letter = str[j];
-      if (letter >= 'a' && letter <= 'z')
+	for (j = 0; j < i; j++)
 	{
-      letter -= 32;
-      str[j] = letter;
+		letter = str[j];
+		if (letter >= 'a' && letter <= 'z')
+		{
+			letter -= 32;
+			str[j] = letter;
+		}
 	}
-    }
 
-  return (str);
+	return (str);
 }
