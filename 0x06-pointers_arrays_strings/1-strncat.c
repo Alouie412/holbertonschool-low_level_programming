f5f657a234b1d96b027085974bc35ff69afc1c20
Fazel412 <alouie412@gmail.com>
2019-10-03 21:56:08 +0000

    Total rewrite

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,14 +10,14 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-  int i, j;
+	int i, j;
 
-  for (i = 0; dest[i] != '\0'; i++);
+	for (i = 0; dest[i] != '\0'; i++);
 
-  for (j = 0; j < n; j++, i++)
-    {
-      dest[i] = src[j];
-    }
+	for (j = 0; j < n; j++, i++)
+	{
+		dest[i] = src[j];
+	}
 
-  return (dest);
+	return (dest);
 }
