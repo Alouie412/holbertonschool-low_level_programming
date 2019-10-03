16d11c44bd7e79b2ac690768586fed779db8088f
Fazel412 <alouie412@gmail.com>
2019-10-03 21:40:19 +0000

    Betty style fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * _strcmp - check the code for Holberton School students.
+ * @s1: First string for comparison
+ * @s2: Second string for comparison
  * Return: Always 0.
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i, value;
-	i = 0;
 
-	while(s1[i] != '\0')
+	i = 0;
+	while (s1[i] != '\0')
 	{
 		if (s1[i] == s2[i])
 		{
