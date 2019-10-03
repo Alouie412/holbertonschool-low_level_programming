a00da944146104a0086c2cecfd32a8de7839c264
Fazel412 <alouie412@gmail.com>
2019-10-03 22:12:23 +0000

    Test file for copying

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,42 +1,46 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * cap_string - check the code for Holberton School students.
+ * @str: String for capitalization modification
  * Return: Always 0.
  */
 char *cap_string(char *str)
 {
-  int i, j, counter;
-  char letter;
+	int i, j, counter;
+	char letter;
 
-  i = counter = 0;
-  while (str[i] != '\0')
-    i++;
+	i = counter = j = 0;
+	while (str[i] != '\0')
+		i++;
 
-  for (j = 0; j < i; j++)
-    {
-      if (counter == 0)
+	if (str[j] == '\t')
+		str[j] = ' ';
+
+	else if (str[j] >= '!' && str[j] <= '"')
+		counter = 0;
+	else if (str[j] >= ',' && str[j] <= '.')
+		counter = 0;
+	else if (str[j] >= '{' && str[j] <= '}')
+		counter = 0;
+	else if (str[j] == ';' || str[j] == '?' || str[j] == '\n' || str[j] == ' ')
+		counter = 0;
+	else if (str[j] == '(' && str[j] == ')')
+		counter = 0;
+	else
+		counter++;
+
+	for (j = 0; j < i; j++)
 	{
-	  letter = str[j];
-	  if (letter >= 'a' && letter <= 'z')
-	    {
-	      letter -= 32;
-	      str[j] = letter;
-	    }
+		if (counter = 0)
+		{
+			letter = str[j];
+			if (letter >= 'a' && letter <= 'z')
+				letter -= 32;
+
+			str[j] = letter;
+		}
 	}
-      else
-	counter++;
 
-      if (str[j] == '\t')
-	str[j] = ' ';
-
-      if ((str[j] >= '(' && str[j] <= ')') || (str[j] >= '!' && str[j] <= '"') || (str[j] >= ',' && str[j] <= '.') || (str[j] >= '{' && str[j] <= '}') || (str[j] == ';') || (str[j] == '?') || (str[j] == ' ') || (str[j] == '\t') || (str[j] == '\n'))
-	counter = 0;
-      else
-	counter ++;
-
-    }
-
-  return (str);
+	return (str);
 }
