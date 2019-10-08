940d5b0dd1b6e845e954a1ddf6095beffeca424b
Fazel412 <alouie412@gmail.com>
2019-10-08 05:18:59 +0000

    Initial commit

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * set_string - check the code for Holberton School students.
+ * @s: Double pointer
+ * @to: Single pointer
+ * Return: Always 0.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
