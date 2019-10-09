36a991f906495da758a3eea3959f75bff39c73c5
Fazel412 <alouie412@gmail.com>
2019-10-09 22:02:20 +0000

    Betty style fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -36,9 +36,11 @@ int sqrt_loop(int n, int m)
  */
 int _sqrt_recursion(int n)
 {
-	if (n == 1 || n == 0)
+	if (n == 1)
 		return (n);
 	else if (n < 0)
+		return (-1);
+	else if (n == 0)
 		return (-1);
 
 	return (sqrt_loop(n, n));
