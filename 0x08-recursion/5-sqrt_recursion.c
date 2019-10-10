831117c27e56520cb6b64abfbf0f305f7aa8caff
Fazel412 <alouie412@gmail.com>
2019-10-10 03:20:43 +0000

    Added lines accounting for very large integers

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,21 @@
 #include "holberton.h"
 
 /**
- * varHolder - Holds a variable to get around recursion issue
- * @num: Holds a number
- *
- * Return: The number minus 1
- */
-int varHolder(int num)
-{
-	num--;
-
-	return (num);
-}
-
-/**
  * sqrt_loop - Finds natural square root, if it exists
  * @n: Original value
  * @m: Determine if original divided by this has no remainder
  * Return: The natural square root, or -1 if one does not exist
  */
-int sqrt_loop(int n, int m)
+int sqrt_loop(long n, long m)
 {
-	if (m == 1)
+	if (m == 1 || m * m > n)
 		return (-1);
-	else if (n / (varHolder(m)) == (varHolder(m)) && (n % (varHolder(m)) == 0))
-		return (n / (varHolder(m)));
+	else if ((n / m == m) && (n % m == 0))
+		return (n / m);
 	else
-		return (sqrt_loop(n, (varHolder(m))));
+	  {
+		return (sqrt_loop(n, m - 1));
+	  }
 }
 
 /**
