47aff1303020929ace933817240d5d8285a346ab
Fazel412 <alouie412@gmail.com>
2019-10-13 21:02:58 +0000

    Logic rewrite

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include "holberton.h"
 
 /**
@@ -11,28 +13,32 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, j, length, sum;
+	char *ptr;
 
-	if (argc == 1)
+	if (argc < 2)
 		printf("0\n");
 	else
 	{
 		sum = 0;
-
 		for (i = 1; i < argc; i++)
 		{
-			if ((*argv[i] >= '0') && (*argv[i] <= '9'))
-				sum += atoi(argv[i]);
+			ptr = argv[i];
+			length = strlen(ptr);
 
-			else
+			for (j = 0; j < length; j++)
 			{
-				printf("Error\n");
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
 					return (1);
+				}
 			}
+
+			sum += atoi(argv[i]);
 		}
-	}
 
 	printf("%d\n", sum);
-
+	}
 	return (0);
 }
