9f2de920609aa4ec592c60d2d8a51bcee6c26c52
Fazel412 <alouie412@gmail.com>
2019-10-17 22:46:03 +0000

    Logic fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(nmemb * size);
+	ptr = malloc(nmemb * size * sizeof(int));
 
 	if (ptr == NULL)
 		return (NULL);
