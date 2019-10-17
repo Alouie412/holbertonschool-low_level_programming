478fe8a9cb8382779d87c14feb774a16e2d43c45
Fazel412 <alouie412@gmail.com>
2019-10-17 21:47:39 +0000

    Fixed typo

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(nmemb * sizeof(size));
+	ptr = malloc(nmemb * size);
 
 	if (ptr == NULL)
 		return (NULL);
