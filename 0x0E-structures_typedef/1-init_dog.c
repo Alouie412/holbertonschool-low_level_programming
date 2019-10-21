11dcce80313681d2cf9ca8ce1807cd824bb8b6ca
Fazel412 <alouie412@gmail.com>
2019-10-21 22:24:49 +0000

    Added conditionals

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,7 +10,12 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if(name)
+		d->name = name;
+
+	if(age)
+		d->age = age;
+
+	if(owner)
+		d->owner = owner;
 }
