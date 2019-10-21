650d69006ff1b41cd39ada8af8e828d5e9fc9c59
Fazel412 <alouie412@gmail.com>
2019-10-21 22:31:45 +0000

    Format fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,16 +12,17 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		printf("\n");
 
+	printf("Name: ");
 	if (d->name == NULL)
 		printf("(nil)\n");
 	else
 		printf("%s\n", d->name);
-
+	printf("Age: ");
 	if (d->age == '\0')
 		printf("(nil)\n");
 	else
 		printf("%f\n", d->age);
-
+	printf("Owner: ");
 	if (d->owner == NULL)
 		printf("(nil)\n");
 	else
