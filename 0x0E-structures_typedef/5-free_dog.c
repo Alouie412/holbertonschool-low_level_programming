c41cf67f8f3e72b2db6dbdaf0f178b530c524fd9
Fazel412 <alouie412@gmail.com>
2019-10-22 06:47:09 +0000

    Added descriptions

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * free_dog - Function that frees memory created in 4-new_dog.c
+ * @d: Input struct
+ * Return: Nothing
+ */
 void free_dog(dog_t *d)
 {
 	if (d != NULL)
