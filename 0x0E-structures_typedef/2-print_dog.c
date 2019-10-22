2eb7ff6ee785e5ca413cedbdbc315c6bc82a5cee
Fazel412 <alouie412@gmail.com>
2019-10-22 00:46:52 +0000

    Changed logic

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,20 +9,20 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		printf("");
+	if (d != NULL)
+	{
+		printf("Name: ");
+		if (d->name == NULL)
+			printf("(nil)\n");
+		else
+			printf("%s\n", d->name);
 
-	printf("Name: ");
-	if (d->name == NULL)
-		printf("(nil)\n");
-	else
-		printf("%s\n", d->name);
+		printf("Age: %f\n", d->age);
 
-	printf("Age: %f\n", d->age);
-
-	printf("Owner: ");
-	if (d->owner == NULL)
-		printf("(nil)\n");
-	else
-		printf("%s\n", d->owner);
+		printf("Owner: ");
+		if (d->owner == NULL)
+			printf("(nil)\n");
+		else
+			printf("%s\n", d->owner);
+	}
 }
