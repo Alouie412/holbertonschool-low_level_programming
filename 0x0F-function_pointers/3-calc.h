7a699bdf747dd09ff45a1c2dc716a56d50c91aea
Fazel412 <alouie412@gmail.com>
2019-10-24 06:59:21 +0000

    Betty style fix

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -6,7 +6,7 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_func(char*s))(int, int);
+int (*get_op_func(char *s))(int, int);
 
 /**
  * struct op - Struct op
@@ -16,8 +16,8 @@ int (*get_op_func(char*s))(int, int);
  */
 typedef struct op
 {
-  char *op;
-  int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 #endif
