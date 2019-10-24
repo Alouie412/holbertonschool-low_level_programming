062b92944ca89aafc51be79fbebe7b666239c49a
Fazel412 <alouie412@gmail.com>
2019-10-24 04:42:10 +0000

    Added conditional statement

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,6 +15,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int i;
 	void (*ptr)(int);
 
+	if (size <= 0)
+		return (NULL);
+
 	ptr = action;
 	for (i = 0; i < size; i++)
 		ptr(array[i]);
