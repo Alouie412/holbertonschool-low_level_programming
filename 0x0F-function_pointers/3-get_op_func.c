d9f66c006d8d47a6d5bfd0e35a20681768ea97d1
Fazel412 <alouie412@gmail.com>
2019-10-24 06:58:23 +0000

    Betty style fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -10,23 +10,24 @@
 int (*get_op_func(char *s))(int, int)
 {
 	/* ops[6][2]*/
-    op_t ops[] = {
-    {"+", op_add},
-    {"-", op_sub},
-    {"*", op_mul},
-    {"/", op_div},
-    {"%", op_mod},
-    {NULL, NULL}
-  };
-  int i;
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i;
 
-  i = 0;
-  while (i < 5)
-    {
-      if (*ops[i].op == *s)
-	  return (ops[i].f);
-      i++;
-    }
+	i = 0;
+	while (i < 5)
+	{
+		if (*ops[i].op == *s)
+			return (ops[i].f);
 
-  return (NULL);
+		i++;
+	}
+
+	return (NULL);
 }
