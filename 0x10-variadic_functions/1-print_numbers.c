e5a90ffda11752e7ae8e6fccbf9e8951730fff8c
Fazel412 <866@holbertonschool.com>
2019-10-26 21:17:14 +0000

    Deleted irrelevant if statement

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,10 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		num = va_arg(int_input, int);
-		if (num == NULL)
-			printf("(nil)");
-		else
-			printf("%d", num);
+		printf("%d", num);
 
 		if ((i != n - 1) && separator != NULL)
 			printf("%c ", *separator);
