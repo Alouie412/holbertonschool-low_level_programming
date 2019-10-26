9dd344cc67d0cf39839df82a847c9ae9044e4483
Fazel412 <866@holbertonschool.com>
2019-10-26 21:37:28 +0000

    Removed unnecessary space

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,7 +2,7 @@
 #define VARIADIC_FUNCTIONS_H
 
 int _putchar(char c);
-int sum_them_all(const unsigned int , ...);
+int sum_them_all(const unsigned int, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
