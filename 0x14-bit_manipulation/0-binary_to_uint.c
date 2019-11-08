8591ba90822b95b06aa1da0b4fb1e3aa3233f7d8
Alouie412 <alouie412@gmail.com>
2019-11-08 22:59:04 +0000

    Added NULL check

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,6 +9,9 @@ unsigned int binary_to_uint(const char *b)
 {
 	int i, mult = 1, count = 0, sum = 0;
 
+	if (b == NULL)
+		return (0);
+
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] == '0' || b[i] == '1')
