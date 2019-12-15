158ccbe12fe1c7c4e784e851e532303a7bf68fda
Alouie412 <alouie412@gmail.com>
2019-12-15 18:40:16 +0000

    Added commentary

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,10 +1,15 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - Function that prints all values in a linked list
+ * @h: The passed in linked list
+ * Return: The number of nodes printed
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count;
 
-	for(count = 0; h != NULL; count++)
+	for (count = 0; h != NULL; count++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
