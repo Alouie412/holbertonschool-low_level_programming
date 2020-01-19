560611d2a94941fe485be9413f00212180a4aee6
Alouie412 <alouie412@gmail.com>
2020-01-19 22:47:22 +0000

    Allowed malloc of size 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,12 +11,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash = NULL;
 
-	if (size <= 0)
+	if (size < 0)
 		return (NULL);
 
-	new_hash = malloc(sizeof(size) * size + sizeof(hash_table_t));
+	new_hash = malloc(sizeof(hash_table_t));
 
 	if (new_hash == NULL)
+		return (NULL);
+
+        new_hash->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (new_hash->array == NULL)
 		return (NULL);
 
 	return (new_hash);
