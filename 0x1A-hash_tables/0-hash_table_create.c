2e409885d75551f4e30ca8410f6c4a0cfdc29991
Alouie412 <alouie412@gmail.com>
2020-01-19 22:49:37 +0000

    Removed if statement: size check

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,9 +11,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash = NULL;
 
-	if (size < 0)
-		return (NULL);
-
 	new_hash = malloc(sizeof(hash_table_t));
 
 	if (new_hash == NULL)
