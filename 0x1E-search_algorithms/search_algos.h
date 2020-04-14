ae26059176db3f80b7ef1908879bc991df86ab19
Alouie412 <alouie412@gmail.com>
2020-04-14 21:33:58 +0000

    Added function prototype: advanced binary + recursive binary

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,5 +9,7 @@ int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int mod_binary_search(int *array, int value, int begin, int end);
+int advanced_binary(int *array, size_t size, int value);
+int recursive_binary(int *array, int value, int begin, int end);
 
 #endif
