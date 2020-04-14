f6f52e3283e991a23e4fab413919fb770f7a53bb
Alouie412 <alouie412@gmail.com>
2020-04-14 21:04:42 +0000

    Added function prototype: exponential search + modified binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,5 +6,8 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int mod_binary_search(int *array, int value, int begin, int end);
 
 #endif
